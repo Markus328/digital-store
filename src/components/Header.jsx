93b4bba3588a047d0574555eb98ae3b5aace5eed
Markus328 <markus328@tutanota.com>
2025-05-25 22:43:05 -0300

    Ajusta a cor do footer e mapeia as cores usadas para as que estão em index.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import NavBar from "../components/NavBar.jsx";
-import Logo from "../assets/logo.png"
-import MiniCart from "../assets/mini-cart.svg"
+import Logo from "../assets/logo.png";
+import MiniCart from "../assets/mini-cart.svg";
 
 const Header = () => {
   return (
@@ -24,18 +24,24 @@ const Header = () => {
               viewBox="0 0 24 24"
               stroke="currentColor "
             >
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M21 21l-4.35-4.35m0 0A7.5 7.5 0 104.5 4.5a7.5 7.5 0 0012.15 12.15z" />
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-4.35-4.35m0 0A7.5 7.5 0 104.5 4.5a7.5 7.5 0 0012.15 12.15z"
+              />
             </svg>
           </div>
-
 
           <div className="flex gap-8 ">
             {/* link para a página de cadastre-se */}
             {/* link para a página de logar*/}
-            <button class=" cursor-pointer text-[16px] font-normal font-['Inter'] text-gray-700 underline underline-offset-4 decoration-1 tracking-[1.00px] text-base leading-7 hover:text-gray-900 transition-colors duration-200">Cadastre-se</button>
-            <button class=" cursor-pointer w-[114px] h-[40px] bg-[#C92071] text-white rounded-lg font-semibold hover:bg-[#e03b8e] transition-colors duration-200">Entrar</button>
-            
+            <button class=" cursor-pointer text-[16px] font-normal font-['Inter'] text-gray-700 underline underline-offset-4 decoration-1 tracking-[1.00px] text-base leading-7 hover:text-gray-900 transition-colors duration-200">
+              Cadastre-se
+            </button>
+            <button class=" cursor-pointer w-[114px] h-[40px] bg-(--primary) text-white rounded-lg font-semibold hover:bg-(--light-primary) transition-colors duration-200">
+              Entrar
+            </button>
           </div>
 
           <div>
