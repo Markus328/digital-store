671cd864649ef381731315a23fb79bf89483c6aa
Markus328 <markus328@tutanota.com>
2025-05-28 21:13:46 -0300

    Adiciona suporte a estilos no Layout

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom";
 import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
 
-const Layout = ({ children }) => {
+const Layout = ({ children, className, style }) => {
   return (
-    <div className="w-full h-full flex flex-col">
+    <div className={`w-full h-full flex flex-col`}>
       <Header />
-      <main className="mt-15">{children}</main>
+      <main className={`mt-15 ${className}`} style={style}>
+        {children}
+      </main>
       <Footer />
     </div>
   );
